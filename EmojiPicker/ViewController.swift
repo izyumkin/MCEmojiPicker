053d62466fb530b179bd3e93819985acbf4de131
izyumkin <izyumkin@gmail.com>
2022-06-01 11:00:47 +0500

    Initial Commit

---
+++ EmojiPicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiPicker
+//
+//  Created by Иван Изюмкин on 01.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
