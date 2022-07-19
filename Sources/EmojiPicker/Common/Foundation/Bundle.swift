a1285057163e4e69559c95c68a06891682d557b2
Egor Badmaev <badmaeve2511@gmail.com>
2022-07-19 23:09:03 +0300

    fix: import Foundation to access the Bundle

--- Sources/EmojiPicker/Common/Foundation/Bundle.swift
+++ Sources/EmojiPicker/Common/Foundation/Bundle.swift
@@ -19,6 +19,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+import Foundation
+
 extension Bundle {
     static var sdkResources: Bundle {
         .init(url: sdkResourcesURL)!
