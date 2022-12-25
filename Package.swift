e70b416662a13aa974060171957e61c4270331f0
izzyumkin <izzyumkin@gmail.com>
2022-12-25 20:12:49 +0500

    Fixed manifest

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "MCEmojiPicker",
     defaultLocalization: "en",
-    platforms: [.iOS(.v11.1)],
+    platforms: [.iOS("11.1")],
     products: [.library(name: "MCEmojiPicker", targets: ["MCEmojiPicker"])],
     targets: [.target(name: "MCEmojiPicker", dependencies: [], path: "Sources/MCEmojiPicker")]
 )
