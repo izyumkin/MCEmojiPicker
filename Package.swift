3a1ca620eea8b227fcc2bc2825b9e4602f35e798
izzyumkin <izzyumkin@gmail.com>
2022-12-25 19:54:46 +0500

    Setup Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,9 @@
+import PackageDescription
+
+let package = Package(
+    name: "MCEmojiPicker",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v10)],
+    products: [.library(name: "MCEmojiPicker", targets: ["MCEmojiPicker"])],
+    targets: [.target(name: "MCEmojiPicker", dependencies: [], path: "Sources/MCEmojiPicker")]
+)
