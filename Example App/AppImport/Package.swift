732f8037b23a93c49118fee5408a14255a1e17b4
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2023-01-13 14:19:35 +0300

    refactor: update example iOS app

--- Example App/AppImport/Package.swift
+++ Example App/AppImport/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["AppImport"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/htmlprogrammist/EmojiPicker", .upToNextMajor(from: "2.0.0"))
+        .package(url: "https://github.com/htmlprogrammist/EmojiPicker", .upToNextMajor(from: "2.0.1"))
     ],
     targets: [
         .target(
