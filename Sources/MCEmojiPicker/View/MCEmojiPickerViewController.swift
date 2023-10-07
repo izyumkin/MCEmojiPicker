fb4117dd8aa32c23ea9b1cdfdc73c984a3ff7c9e
Raimis <raimis@pinstudios.com>
2023-10-07 13:21:56 +0300

    Add option for callback

--- Sources/MCEmojiPicker/View/MCEmojiPickerViewController.swift
+++ Sources/MCEmojiPicker/View/MCEmojiPickerViewController.swift
@@ -27,6 +27,8 @@ public protocol MCEmojiPickerDelegate: AnyObject {
     func didClose()
 }
 
+public typealias MCEmojiPickerCompletion = (String?) -> Void
+
 public final class MCEmojiPickerViewController: UIViewController {
     
     // MARK: - Public Properties
@@ -96,9 +98,12 @@ public final class MCEmojiPickerViewController: UIViewController {
         return MCEmojiPickerView(categoryTypes: categories, delegate: self)
     }()
     
+    private var completion: MCEmojiPickerCompletion?
+    
     // MARK: - Initializers
     
-    public init() {
+    public init(completion: MCEmojiPickerCompletion? = nil) {
+        self.completion = completion
         super.init(nibName: nil, bundle: nil)
         setupPopoverPresentationStyle()
         setupDelegates()
@@ -129,6 +134,7 @@ public final class MCEmojiPickerViewController: UIViewController {
     public override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         delegate?.didClose()
+        completion?(nil)
         NotificationCenter.default.post(name: .MCEmojiPickerDidDisappear, object: nil)
     }
     
@@ -139,7 +145,11 @@ public final class MCEmojiPickerViewController: UIViewController {
             guard let emoji = emoji else { return }
             feedbackImpactOccurred()
             delegate?.didGetEmoji(emoji: emoji.string)
+            completion?(emoji.string)
+            
             if isDismissAfterChoosing {
+                //clear completion so that it's not called mutltiple times
+                completion = nil
                 dismiss(animated: true, completion: nil)
             }
         }
