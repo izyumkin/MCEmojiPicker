e18606bf23fdbc98fccc35428104309647f6834f
Manu @StuFFmc Carrasco Molina <m+git@carrascomolina.com>
2025-01-21 21:20:01 +0100

    SwiftUI Sample App: Slider for iOS Version and Toggle for Display Count

--- Example App/SwiftUI App/App.swift
+++ Example App/SwiftUI App/App.swift
@@ -13,22 +13,34 @@ struct MCEmojiPicker: App {
 struct ContentView: View {
     @State private var isPresented = false
     @State private var selectedEmoji = "🙋🏻‍♂️"
+    @State private var maxCurrentAvailableOsVersion: Float = 18
+    @State private var displayCountOfEmojisInHeader = true
 
     var body: some View {
-        VStack {
-            Button(selectedEmoji) {
-                isPresented.toggle()
-            }.emojiPicker(
-                isPresented: $isPresented,
-                selectedEmoji: $selectedEmoji
-            )
-            .padding(5)
-            .background(Color.gray.opacity(0.3))
-            .cornerRadius(10)
-            .font(.system(size: 65))
-            Spacer()
+        NavigationStack {
+            VStack {
+                Slider(value: $maxCurrentAvailableOsVersion, in: 10...18, step: 1)
+                Toggle("Display Count", isOn: $displayCountOfEmojisInHeader)
+                Button(selectedEmoji) {
+                    isPresented.toggle()
+                }.emojiPicker(
+                    isPresented: $isPresented,
+                    selectedEmoji: $selectedEmoji,
+                    isDismissAfterChoosing: false,
+                    maxCurrentAvailableOsVersion: maxCurrentAvailableOsVersion,
+                    displayCountOfEmojisInHeader: displayCountOfEmojisInHeader
+                )
+                .padding(5)
+                .background(Color.gray.opacity(0.3))
+                .cornerRadius(10)
+                .font(.system(size: 65))
+                Spacer()
+            }
+            .padding()
+            .background(.secondary.opacity(0.2))
+            .navigationTitle("Showing Emojis for iOS \(Int(maxCurrentAvailableOsVersion))")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding(30)
     }
 }
 
