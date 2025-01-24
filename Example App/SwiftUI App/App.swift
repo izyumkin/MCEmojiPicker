fed2d2a77f7fd7e086a22a63b53c4d6feab53a80
Manu @StuFFmc Carrasco Molina <m+git@carrascomolina.com>
2025-01-24 16:55:04 +0100

    Updating SwiftUI Sample App for using the new params + ShareLink

--- Example App/SwiftUI App/App.swift
+++ Example App/SwiftUI App/App.swift
@@ -13,30 +13,39 @@ struct MCEmojiPicker: App {
 struct ContentView: View {
     @State private var isPresented = false
     @State private var selectedEmoji = "🙋🏻‍♂️"
-    @State private var maxCurrentAvailableOsVersion: Float = 18
+    @State private var maxCurrentAvailableOsVersion: Float = 17
     @State private var displayCountOfEmojisInHeader = true
+    @State private var onlyShowNewEmojisForVersion = true
 
     var body: some View {
         NavigationStack {
             VStack {
                 Slider(value: $maxCurrentAvailableOsVersion, in: 10...18, step: 1)
-                Toggle("Display Count", isOn: $displayCountOfEmojisInHeader)
-                Button(selectedEmoji) {
-                    isPresented.toggle()
-                }.emojiPicker(
-                    isPresented: $isPresented,
-                    selectedEmoji: $selectedEmoji,
-                    isDismissAfterChoosing: false,
-                    maxCurrentAvailableOsVersion: maxCurrentAvailableOsVersion,
-                    displayCountOfEmojisInHeader: displayCountOfEmojisInHeader
-                )
-                .padding(5)
-                .background(Color.gray.opacity(0.3))
-                .cornerRadius(10)
+                    .padding(.horizontal)
+                HStack(spacing: 40) {
+                    Toggle("Count", isOn: $displayCountOfEmojisInHeader)
+                    Toggle("Only new", isOn: $onlyShowNewEmojisForVersion)
+                }
+                .padding(.horizontal)
+                HStack {
+                    Button(selectedEmoji) {
+                        isPresented.toggle()
+                    }.emojiPicker(
+                        isPresented: $isPresented,
+                        selectedEmoji: $selectedEmoji,
+                        isDismissAfterChoosing: false,
+                        maxCurrentAvailableOsVersion: maxCurrentAvailableOsVersion,
+                        displayCountOfEmojisInHeader: displayCountOfEmojisInHeader,
+                        onlyShowNewEmojisForVersion: onlyShowNewEmojisForVersion
+                    )
+                    .padding(5)
+                    .background(Color.gray.opacity(0.3))
+                    .cornerRadius(10)
+                    ShareLink("", item: selectedEmoji)
+                }
                 .font(.system(size: 65))
                 Spacer()
             }
-            .padding()
             .background(.secondary.opacity(0.2))
             .navigationTitle("Showing Emojis for iOS \(Int(maxCurrentAvailableOsVersion))")
             .navigationBarTitleDisplayMode(.inline)
